e222b56bce3e66d14b1a685e64697b31e7311baf
Pat Orders <pat.orders@gmail.com>
2012-02-23 22:54:15 -0700

    second commit

--- lib/siriproxy-channels.rb
+++ lib/siriproxy-channels.rb
@@ -3,9 +3,7 @@ require 'siri_objects'
 require 'pp'
 
 class SiriProxy::Plugin::Example < SiriProxy::Plugin
-    def initialize(config)
-        #if you have custom configuration options, process them here!
-    end
+    
     listen_for /test my channels/i do
       say "All is well, Pat"
     

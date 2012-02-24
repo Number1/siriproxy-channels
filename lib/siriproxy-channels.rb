cf78f8754509b29a63c490b0f4f1d1a5fb189d90
Pat Orders <pat.orders@gmail.com>
2012-02-23 22:32:15 -0700

    second commit

--- lib/siriproxy-channels.rb
+++ lib/siriproxy-channels.rb
@@ -1,60 +1,14 @@
+require 'cora'
 require 'siri_objects'
-require 'nokogiri'
-require 'open-uri'
-require'cora'
 require 'pp'
-#stopped at 52
 
 
- 
-def channel_check(channel)
-
-    tempo = Time.new
-
-h = [0, 0, 10780, 10839, 0, 10603, 73442, 21343, 10734, 63705,
-    10535, 63823, 10659, 32892, 11069, 46256, 22130, 23325, 
-    23326, 22133, 23896, 23897, 10816, 10342, 16715, 11867, 
-    53324, 11221, 10183, 11207, 10918, 10145, 23340, 10179,
-    12444, 11059, 10142, 16300, 16374, 10989, 10153, 52277, 
-    21484, 12574, 14902, 11180, 11150, 16331, 14771, 11158, 
-    10057, 10035, 14753, 10139, 44263, 16011, 15952, 15377,
-    10021, 11164, 11187, 0,     10150, 16123, 11163, 10138, 
-    10986, 11218, 18511, 11066, 17098, 11097, 10269, 10051, 
-    10093, 11006, 12510, 18151, 10161, 10162, 10380]
-
-    number = h[channel]
-
-uri = "http://tvlistings.zap2it.com/tvlistings/ZCSGrid.do?stnNum=#{number}&channel=#{channel}"
-doc = Nokogiri::HTML(open(uri))
-
-
-channel1 = doc.css("h1").map do |status|
-    status.text.strip
+class SiriProxy::Plugin::Example < SiriProxy::Plugin
+    def initialize(config)
+        #if you have custom configuration options, process them here!
     end
-
-program = doc.css('a[id="rowTitle1"]').map do |prog1|
-    prog1.text.strip
-    end
-
-    if (tempo.min >= 30 && tempo.min <=55)
-        program1=program[2]
-    else
-        program1=program[1]
-    end
-
-channel2=channel1[0]
-
-say "currently playing "
-    request_completed
-
-    #print "channel ", channel, " ", channel2, " ", program1, "\n"
-    end ##check_channel
-
-    listen_for /Whats playing on channel/i do #|number|
-    say "Hi Pat. How are you?"
-    request_completed
-    #check_channel(number)
-    end
-
-
-
+    listen_for /test my channels/i do
+      say "All is well, Pat"
+    
+      request_completed
+end
